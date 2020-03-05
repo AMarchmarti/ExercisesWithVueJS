81bf7b631ecb707cc62744e37fff972944a1f290
Antoni March Marti <marchmarti.antoni@gmail.com>
2020-03-05 20:31:35 +0100

    v-for and v-if-else implemented

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = new Vue({
-    el: '#app',
-    data: {
-        titulo: 'Hola Mundo con Vue'
-    }
-})
+  el: "#app",
+  data: {
+    titulo: "Hola Mundo con Vue",
+    frutas: [
+      { nombre: "Manzana", cantidad: 10 },
+      { nombre: "Pera", cantidad: 0 },
+      { nombre: "Platano", cantidad: 27 }
+    ]
+  }
+});
