235e85bd4805bf862aced948ad9f5d4b412cd88f
Antoni March Marti <marchmarti.antoni@gmail.com>
2020-03-06 22:14:10 +0100

    dinamic parameters and route in events

--- Exercises_7.VueCLI_Router/project-router/src/views/Photos.vue
+++ Exercises_7.VueCLI_Router/project-router/src/views/Photos.vue
@@ -1,17 +1,46 @@
 <template>
-    <div>
-        <h1>Galery</h1>
-        <Photo />
-    </div>
+  <div>
+    <h1>Galery</h1>
+
+    <router-link
+      :to="{ name: 'Photos', params: { id: photo } }"
+      v-for="(photo, index) of photos"
+      :key="index"
+    >
+      <button>photo {{ photo }}</button>
+    </router-link>
+
+    <Photo />
+
+
+    <button @click="home">Home</button>
+    <button @click="before">Before</button>
+    <button @click="next">Next</button>
+  </div>
 </template>
 
 <script>
-
-import Photo from '@/components/Photo.vue'
+import Photo from "@/components/Photo.vue";
 
 export default {
-    components:{
-        Photo
-    }
-}
+  components: {
+    Photo
+  },
+  data() {
+    return {
+      photos: [1, 2, 3]
+    };
+  },
+  methods: {
+      home(){
+          this.$router.push('/')
+      },
+      before(){
+          this.$router.go(-1)
+      },
+      next(){
+          this.$router.go(1)
+      }
+  },
+};
 </script>
