9c95eee37685f25ac9602b3f4031e3aab399bc0b
Antoni March Marti <marchmarti.antoni@gmail.com>
2020-03-06 21:50:16 +0100

    added new route to practice

--- Exercises_7.VueCLI_Router/project-router/src/router/index.js
+++ Exercises_7.VueCLI_Router/project-router/src/router/index.js
@@ -8,15 +8,17 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/service',
+    name: 'Service',
+    component: () => import(/* webpackChunkName: "service" */ '../views/Service.vue')
   }
 ]
 
